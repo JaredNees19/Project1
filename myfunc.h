cdcb5348e94a2d4deedf08118a035954ab3c3dd5
Jared.Nees.19 <jared.nees.19@cnu.edu>
2021-09-11 16:04:25 -0400

    adding myfunc cpp and myfunc header files

---
+++ myfunc.h
@@ -0,0 +1,8 @@
+//#includes are ALWAYS at top of file
+#ifndef MYFUNC_H_
+#define MYFUNC_H_
+#include <iostream>	//iostream is where std::cout and std::endl are defined
+
+std::string func();
+
+#endif/* MYFUNC_H_*/
